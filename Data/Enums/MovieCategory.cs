de9d666e93de7fabe816dbcaa8f3c9127877ae95
smik91 <125355749+smik91@users.noreply.github.com>
2024-02-26 19:56:46 +0300

    Initial

---
+++ Data/Enums/MovieCategory.cs
@@ -0,0 +1,12 @@
+﻿namespace pet_project.Data.Enums
+{
+    public enum MovieCategory
+    {
+        Action = 1,
+        Comedy,
+        Drama,
+        Documentary,
+        Horror,
+        Cartoon
+    }
+}
---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace pet_project.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
